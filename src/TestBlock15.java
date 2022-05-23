9498a9b5fb024e36c54347a9bff60497376e1acf
aydinmhrrm <aydin.mhrrm16@gmail.com>
2022-05-23 16:25:19 -0400

    Bootcamp (Java)

---
+++ src/TestBlock15.java
@@ -0,0 +1,20 @@
+public class TestBlock15 {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+        System.out.println("******");
+
+
+        System.out.println("""
+                *
+                **
+                ***
+                ****
+                *****
+                               
+                """);
+    }
+}
